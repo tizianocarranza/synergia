b8e5ebcbf6295a846dfd4112fd7caadb0fe65a7d
tizianocarranza <tizianocarranza0910@gmail.com>
2024-08-27 11:18:42 -0300

    Navbar redesign

--- src/app/ui/navbar/nav-links.tsx
+++ src/app/ui/navbar/nav-links.tsx
@@ -2,6 +2,8 @@
 
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import Image from "next/image"
+import { useState } from "react"
 
 export const SynergiaLink = () => {
     return (
@@ -51,29 +53,71 @@ export const SignOutLink = () => {
     )
 }
 
-export default function NavLinks() {
+/* TODO */
+/* export function MobileLinks() {
+    const [mobileMenuIsActive, setMobileMenuIsActive] = useState(false);
+
+    const toggleMenu = () => setMobileMenuIsActive(!mobileMenuIsActive);
+
+    const links = [
+        { name: "Home", href: "/", icon: "" },
+        { name: "About", href: "/about", icon: "" },
+        { name: "Inbox", href: "/inbox", icon: "" },
+        { name: "Account", href: "/account", icon: "" },
+    ];
+
+    return (
+        <div className={`lg:hidden flex flex-col flex-1 justify-center items-center gap-10 ${mobileMenuIsActive ? "h-screen w-screen" : "h-full w-full"}`}>
+            <div className={`flex flex-col ${mobileMenuIsActive ? "h-screen w-screen" : "h-full w-full"}`}>
+                <div>
+                    <Image 
+                        src={mobileMenuIsActive ? "/close.svg" : "/navIcon.svg"} 
+                        alt={mobileMenuIsActive ? "Close menu button" : "Open menu button"}
+                        height={25} 
+                        width={25}
+                        onClick={toggleMenu}
+                        aria-expanded={mobileMenuIsActive} // Accesibilidad: indica si el menú está abierto
+                        aria-label={mobileMenuIsActive ? "Close menu" : "Open menu"} // Accesibilidad: etiqueta del botón
+                    />
+                </div>
+
+                <div className={`${mobileMenuIsActive ? "flex" : "hidden"} flex-col gap-5 mt-5`}>
+                    {links.map((link) => (
+                        <Link
+                            key={link.name}
+                            href={link.href}
+                            className="hover-underline"
+                            onClick={toggleMenu} // Cerrar el menú al hacer clic en un enlace
+                        >
+                            {link.name}
+                        </Link>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+} */
+
+
+export function NavLinks() {
     const links = [
         { name: "Home", href: "/", icon: "" },
         { name: "About", href: "/about", icon: "" },
         { name: "Inbox", href: "/inbox", icon: "" },
         { name: "Account", href: "/account", icon: "" },
     ]
-
-    const pathname = usePathname();
-
     return (
-        <div className="flex flex-1 justify-center items-center gap-10">
-            {
-                links.map((link) => {
-                    return (
-                        <Link
-                            key={link.name}
-                            href={link.href}
-                            className="hover-underline"
-                        >{link.name}</Link>
-                    )
-                })
-            }
+        <div className="flex flex-row flex-1 justify-center items-center gap-10">
+            {links.map((link) => {
+                return (
+                    <Link
+                        key={link.name}
+                        href={link.href}
+                        className="hover-underline"
+                    >{link.name}</Link>
+                )
+            })}
         </div>
     )
 }
+
--- src/app/ui/navbar/navbar.tsx
+++ src/app/ui/navbar/navbar.tsx
@@ -1,6 +1,6 @@
 import { headers } from "next/headers";
 import { auth, signOut } from "../../../../auth"
-import NavLinks, { SignInLink, SignOutLink, SynergiaLink } from "./nav-links"
+import { NavLinks, SignInLink, SignOutLink, SynergiaLink } from "./nav-links"
 
 export default async function Navbar() {
     let session = await auth();
@@ -9,18 +9,31 @@ export default async function Navbar() {
 
     return (
         <header className="fixed flex w-screen h-14 min-h-14 justify-center items-center top-0 left-0 z-50 text-sm font-semibold bg-black bg-opacity-75 backdrop-blur-lg">
-            <div className="grid grid-cols-3 items-center w-11/12 h-full  list-none">
-                <SynergiaLink />
+            {/* Desktop */}
+            <div className="grid grid-cols-1 lg:grid-cols-3 items-center w-11/12 h-full  list-none">
+                <div className="hidden lg:block">
+                    <SynergiaLink />
+                </div>
+
                 <NavLinks />
-                { (
+
+                <div className="hidden lg:block">
+                {(
                     session?.user ? (
                         <form action={async () => { "use server"; await signOut() }} className="flex flex-1 justify-end items-center gap-10 text-sm">
-                            <SignOutLink />
-                        </form>
-                    ) : <SignInLink />
-                )
-                }
+                                <SignOutLink />
+                            </form>
+                        ) : <SignInLink />
+                    )}
+                </div>
             </div>
+
+            {/* Mobile TODO MAYBE aunque esta bien ahi */}   
         </header>
     )
 }
+
+
+/* 
+
+*/
