ecfe6f4ea6f200110037f610018498efa30dac54
tizianocarranza <tizianocarranza0910@gmail.com>
2024-10-08 18:48:47 -0300

    Synergia full redesign

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -9,7 +9,7 @@ export default function Account() {
                 <p>Synergia is an innovative web application designed to connect and enhance <span className="text-brand">collaboration</span> between developers and organizations in the tech industry.</p>
                 <p>Through an intuitive interface and advanced tools, Synergia allows <span className="text-brand">developers</span> to showcase their skills, specialties, and experiences through customizable profiles.</p>
                 <p><span className="text-brand">Organizations,</span> on the other hand, can efficiently search for and engage with top talent, streamlining the hiring process.</p>
-                <p>Whether you’re a developer looking to expand your career or you represent an organization seeking the best candidates, Synergia provides the platform to <span className="text-brand">synergize</span> and succeed.</p> 
+                <p>Whether you&apos;re a developer looking to expand your career or you represent an organization seeking the best candidates, Synergia provides the platform to <span className="text-brand">synergize</span> and succeed.</p> 
         </MainPage>
 
     );
